a14ac22b215dad71b8b8caa4f51638143fc95be5
AzlanP <pablo.1994.azlan@gmail.com>
2019-09-04 14:49:33 -0300

    se añadio los componentes necesarios
    componentes:
     my orders,
     admin/orders
     admin/products
    products
    login
    check-out
    shopping-cart
    navbar
    home
    -----
    tambien se añadio un nuevo paquete ng-bootstrap/ng-bootstrap
    y se modifico el nav , se cambio el dropdown

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCJ7VKkYyioQwSt50nQdgOMjZxFKupPeqc",
+    authDomain: "e-commerce-3c1b9.firebaseapp.com",
+    databaseURL: "https://e-commerce-3c1b9.firebaseio.com",
+    projectId: "e-commerce-3c1b9",
+    storageBucket: "",
+    messagingSenderId: "747429220162",
+    appId: "1:747429220162:web:351d33ea41c8db14"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCJ7VKkYyioQwSt50nQdgOMjZxFKupPeqc",
+    authDomain: "e-commerce-3c1b9.firebaseapp.com",
+    databaseURL: "https://e-commerce-3c1b9.firebaseio.com",
+    projectId: "e-commerce-3c1b9",
+    storageBucket: "",
+    messagingSenderId: "747429220162",
+    appId: "1:747429220162:web:351d33ea41c8db14"
+  }
 };
 
 /*
